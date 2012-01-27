d7f81819f2f057bb1fbc1634c7c8772534520f69
Michiel van Vlaardingen <michiel@2vi.nl>
2012-01-27 09:12:48 +0100

    added some empty files required in the future

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+maintainer       "MovingLabs"
+maintainer_email "info@movinglabs.com"
+license          "All rights reserved"
+description      "Installs/Configures The Observu Agent"
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: observu-agent-cookbook
+# Recipe:: default
+#
+# Copyright 2012, MovingLabs
+#
+# All rights reserved - Do Not Redistribute
+#
